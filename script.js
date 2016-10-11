ae50ae2eb8222fcd15400b456b0cd0b22095bd95
haakensonb <untamedhawk44@gmail.com>
2016-10-10 20:55:02 -0400

    Add searchbar and styling

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function searchOnEnter(){
+  if (event.keyCode == 13 || event.which == 13) {
+    location='http://www.google.com/search?q=' + encodeURIComponent(document.getElementById('textbox').value);
+    document.getElementById('textbox').value = '';
+  }
+}
+
 $(document).ready(function(){
   $(".category").click(function() {
     $category = $(this);
